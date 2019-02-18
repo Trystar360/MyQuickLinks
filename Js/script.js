d4456829ba6a938830da3406e0f149eaf911ce60
Trystar360 <tristanfrederick360@gmail.com>
2019-02-18 02:25:55 -0500

    update

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+
 $(".tab").click(function(){
     $(".tab").removeClass("is-active");
     $("#" + $(this).attr("id")).addClass("is-active");
@@ -5,10 +6,60 @@ $(".tab").click(function(){
 });
 
 function showTab(x){
-    $(".hero-body").removeClass("active");
-    $("." + x).addClass("active");
+    $(".hero-body").attr("id", "");
+    $("." + x).attr("id", "htab");
 }
 
 function changeColor(x){
     $(".colors").addClass(x);
+}
+
+function loadUserData(){
+    for(var i = 1; i < 5; i++){
+        $("#tblk" + i).html("<a>"+userData["gsx$classname" + i]["$t"] + "</a>");
+        console.log(userData["gsx$classname" + i]["$t"]);
+    }
+
+
+}
+
+if(localStorage.getItem("email") == null || localStorage.getItem("email") == "null"){
+    localStorage.setItem("email","default");
+}
+checkEmail(localStorage.getItem("email"));
+function checkEmail(cur){
+    console.log(cur);
+    if(cur == "default"){
+        $("#emailModal").addClass("is-active");
+    }else{
+        getSheetData();
+        $("#navSignUp").addClass("is-hidden");
+    $("#navLogIn").addClass("is-hidden");
+    $("#navLogOut").removeClass("is-hidden");
+    }
+}
+
+function closeEmail(){
+    $("#emailModal").removeClass("is-active");
+}
+
+function login(){
+    var input = $("#emailInput").val();
+    console.log(input);
+    localStorage.setItem("email" , input);
+    $("#emailInput").val("");
+    closeEmail();
+    getSheetData();
+    $("#navSignUp").addClass("is-hidden");
+    $("#navLogIn").addClass("is-hidden");
+    $("#navLogOut").removeClass("is-hidden");
+}
+
+
+function logOut(){
+    localStorage.setItem('email','default');
+     $('#emailModal').addClass('is-active');
+     $("#navSignUp").removeClass("is-hidden");
+    $("#navLogIn").removeClass("is-hidden");
+    $("#navLogOut").addClass("is-hidden");
 }
